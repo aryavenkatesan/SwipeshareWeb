66f06fbb6c9455e01692a20192ce617e3cca2b66
Arya Venkatesan <ar6165@gmail.com>
2025-09-27 13:27:31 -0400

    file structure

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // @ts-expect-error - types not ready yet
   plugins: [react(), tailwindcss()],
-  base: '/SwipeshareWeb/',
+  base: '/',
 })
